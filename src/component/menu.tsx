5de5ecac194269e574e320efde76effbbbad4e17
Ath <ath@MacBook-de-Ath.local>
2019-04-07 14:37:30 +0200

    Menu to appbar

--- src/component/menu.tsx
+++ src/component/menu.tsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Dashboard from '@material-ui/icons/Dashboard';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 interface Props {
 
@@ -18,14 +23,17 @@ export default class Menu extends Component<Props, State> {
   render() {
       return (
         <>
-          <div className={"menu"}>
-          <Typography className={"menu-title"}>
-          <Dashboard></Dashboard>
-            Projects Dashboard
-          </Typography>
-          </div>
-          <div className={"left-panel"} >
-          </div>
+        <AppBar className={"menu"}>
+          <Toolbar>
+            <IconButton color="inherit" className={"menu-icon"}>
+              <Dashboard/>
+            </IconButton>
+            <Typography variant="h6" color="inherit" className={"menu-label"}>
+              Projects Dashboard
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <div className={"left-panel"}></div>
         </>
       );
   }
